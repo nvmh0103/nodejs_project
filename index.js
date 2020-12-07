986629fd16428b3c6b3859690cf4cfcef2b20fe1
nvmh0103 <19520564@gm.uit.edu.vn>
2020-12-07 20:07:53 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const morgan=require('morgan')
 const app = express()
 const port=3000
- 
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   res.send('Hello World iam gonna change the world')
 })
