93d62e60f390670f099d8ba9f0bbd1ba6196b1bb
nvmh0103 <19520564@gm.uit.edu.vn>
2020-12-07 19:46:43 +0700

    2nd attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port=3000
  
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World iam gonna change the world')
 })
  
 app.listen(port,() => console.log(`Listening at http://localhost:${port}`))
