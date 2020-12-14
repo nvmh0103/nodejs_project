3158e1f2e2c70520b4e743ebc3c49652a7f2c9e9
nvmh0103 <19520564@gm.uit.edu.vn>
2020-12-14 20:32:17 +0700

    m

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ app.use(express.static(path.join(__dirname,'public')));
 
 //HTTP logger
 app.use(morgan('combined'))
+
 //template engine
 app.engine('hbs',handlebars({
   extname:'.hbs'
 }))
+
 app.set('view engine','hbs')
+
 app.set('views',path.join(__dirname,'resources/views'))
 
 app.get('/news', (req, res) => {
