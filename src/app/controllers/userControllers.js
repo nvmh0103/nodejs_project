edb7e11cba83a5fce2113d35d7c4731399942d05
nvmh0103 <19520564@gm.uit.edu.vn>
2020-12-20 15:45:37 +0700

    add deleted count

--- src/app/controllers/userControllers.js
+++ src/app/controllers/userControllers.js
@@ -4,11 +4,29 @@ const util = require('../../util/mongoose.js');
 class userControllers {
     // get /home
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(Courses=>{
-                res.render('user/storedCourses',{Courses:util.arrayToObject(Courses)});
+        
+        Promise.all([Course.find({}),Course.countDocumentsDeleted()])
+            .then(([Courses,deletedCount])=>{
+                res.render('user/storedCourses',{
+                    deletedCount,
+                    Courses:util.arrayToObject(Courses)});
             })
             .catch(next);
+
+        // Course.countDocumentsDeleted()
+        //     .then((deletedCount)=>{
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(()=>{
+
+        //     });
+        
+        
+        // Course.find({})
+        //     .then(Courses=>{
+        //         res.render('user/storedCourses',{Courses:util.arrayToObject(Courses)});
+        //     })
+        //     .catch(next);
     }
     // trash courses
     trashCourses(req,res,next){
